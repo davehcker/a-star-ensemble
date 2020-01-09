cd84c9eab080338492ec57a0dc45ba317862bbce
david.ahmed <david.a@pigeonline.io>
2020-01-09 15:31:03 +0100

    removed old file structure after rename

--- a-star/optimizer.py
+++
@@ -1,8 +0,0 @@
-def optimize(x, y, models,
-             criteria=None):
-    """Return an optimized version of models based on the criteria
-
-    Returns:
-        models (list): a sorted list of best classifying models.
-    """
-    pass
